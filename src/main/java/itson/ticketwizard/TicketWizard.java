7c161f28c7e15eaa94801ef7fe4f5b92f5cee26f
RaulNav <ari.montoya252825@potros.itson.edu.mx>
2025-02-18 20:53:05 -0700

    Probando

--- src/main/java/itson/ticketwizard/TicketWizard.java
+++ src/main/java/itson/ticketwizard/TicketWizard.java
@@ -11,6 +11,6 @@ package itson.ticketwizard;
 public class TicketWizard {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello!");
     }
 }
