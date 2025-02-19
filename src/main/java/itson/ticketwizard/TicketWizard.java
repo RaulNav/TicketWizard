221a68d73805ef9a7f6e844dac33cbe60e44a366
RaulNav <ari.montoya252825@potros.itson.edu.mx>
2025-02-18 20:44:12 -0700

    Agregando proyecto al repositorio

---
+++ src/main/java/itson/ticketwizard/TicketWizard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.ticketwizard;
+
+/**
+ *
+ * @author rauln
+ */
+public class TicketWizard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
